d0d2bb3899c39fc32c39e610453a0ad01d3f78d9
Farmwell <g.ferowich@gmail.com>
2019-12-23 14:24:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2; 
+
+let random = Math.random() * 100;
+let mod = 12 % 8;
+let max = Math.max( 1, 2, 3, 16, 17, 18, 19, 20);
